afaf67d2c67aa26eb81c0eae270cb04236145e55
Srikanth Naik <48707298+Sriku010203@users.noreply.github.com>
2024-03-11 13:52:25 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
+public class Product{
+  public int product(int x,int y){
+  }
+  public int product(int x,int y,int z){
+  }
+  public double product(double x,double y){
+  }
 
 public class Main {
+  Product p=new Product(1,2);
+  
+  
 
 }
